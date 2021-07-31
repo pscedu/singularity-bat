9d7ec912a59c63882fa39d36d3ecc94450a78738
icaoberg <icaoberg@andrew.cmu.edu>
2021-07-30 22:44:17 -0400

    Update test.sh

--- 0.17.1/test.sh
+++ 0.17.1/test.sh
@@ -7,4 +7,4 @@ echo "Testing bat"
 singularity exec singularity-bat-0.17.1.sif bat --help
 
 wget -nc https://raw.githubusercontent.com/pscedu/singularity-bat/main/README.md
-singularity exec singularity-bat-0.17.1.sif bat README.md
+cat README.md | singularity exec singularity-bat-0.17.1.sif bat
