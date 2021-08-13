057c8292a2c04cc6acca9a2cc9ea8acb3391e558
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2021-08-13 13:06:32 -0400

    Added bat 0.18.1

---
+++ 0.18.1/rbuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
+PACKAGE=bat
+VERSION=0.18.1
+IMAGE=singularity-$PACKAGE-$VERSION.sif
+DEFINITION=Singularity
+
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
+singularity build --remote $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
---
+++ 0.18.1/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Listing files"
+tree .
+
+echo "Testing bat"
+singularity exec singularity-bat-0.17.1.sif bat --help
+
+wget -nc https://raw.githubusercontent.com/pscedu/singularity-bat/main/README.md
+cat README.md | singularity exec singularity-bat-0.17.1.sif bat
