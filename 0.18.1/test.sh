5d86476814e4ea5431ca12af78c395a0db42e967
icaoberg <icaoberg@andrew.cmu.edu>
2021-08-14 18:16:46 -0400

    Update test.sh

--- 0.18.1/test.sh
+++ 0.18.1/test.sh
@@ -4,7 +4,7 @@ echo "Listing files"
 tree .
 
 echo "Testing bat"
-singularity exec singularity-bat-0.17.1.sif bat --help
+singularity exec singularity-bat-0.18.1.sif bat --help
 
 wget -nc https://raw.githubusercontent.com/pscedu/singularity-bat/main/README.md
-cat README.md | singularity exec singularity-bat-0.17.1.sif bat
+cat README.md | singularity exec singularity-bat-0.18.1.sif bat
