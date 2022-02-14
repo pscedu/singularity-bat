525cc79a3d28410a4bae9a55178d88686e0d1a9c
Ivan Cao-Berg <icaoberg@andrew.cmu.edu>
2022-02-14 15:39:33 -0500

    Added version 0.18.3

---
+++ 0.18.3/modulefile.lua
@@ -0,0 +1,39 @@
+--
+-- bat 0.18.3 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: bat is a cat clone with wings."
+-- "Keywords: singularity utilities"
+
+whatis("Name: bat")
+whatis("Version: 0.18.3")
+whatis("Category: Other")
+whatis("Description: bat is a cat clone with wings.")
+
+help([[
+bat is a cat clone with wings.
+  
+To load the module, type
+
+> module load bat/0.18.3
+
+To unload the module, type
+
+> module unload bat/0.18.3
+
+Documentation
+-------------
+For help, type
+
+> bat --help
+
+Tools included in this module are
+
+* bat
+]])
+
+local package = "bat"
+local version = "0.18.3"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
