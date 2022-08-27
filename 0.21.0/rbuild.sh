3badf5e3f926cfce211c731766c662beb1e9a6b5
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2022-08-26 20:15:26 -0400

    Added version 0.21.0

---
+++ 0.21.0/rbuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright © 2022 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
+PACKAGE=bat
+VERSION=0.21.0
+IMAGE=singularity-$PACKAGE-$VERSION.sif
+DEFINITION=Singularity
+
+if [ -f $IMAGE ]; then
+	rm -fv $IMAGE
+fi
+
+singularity build --remote $IMAGE $DEFINITION
+
+if [ -f $IMAGE ]; then
+	exit 0
+else
+	exit 1
+fi
---
+++ 0.21.0/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tree .
+
+singularity exec singularity-bat-0.21.0.sif bat --help
+wget -nc https://raw.githubusercontent.com/pscedu/singularity-bat/main/README.md
+cat README.md | singularity exec singularity-bat-0.21.0.sif bat
